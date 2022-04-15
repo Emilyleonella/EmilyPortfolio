89c3b7d17c985e03af39c2c064ad13b31a65a249
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-15 11:54:30 -0400

    added hero to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import heroimage from './assests/heroImg.jpg'
+import Hero from './components/Hero';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Hero imageSrc={heroimage}/>
     </div>
   );
 }
