cf8a8d1557ec77f7d1561c5b2ddcf3dd88ff3861
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-15 20:30:09 -0400

    added slider function and styling

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import '../partials/_slider.scss'
 
-const Slider = () => {
-  return (
-    <div>Slider</div>
-  )
-}
+const Slider = ({ imageSrc, title, subtitle, flipped }) => {
+  const renderContent = () => {
+    if (!flipped) {
+      return (
+        <>
+          <img src={imageSrc} alt="Emily_Picture" className="imgOne" />
+          <div className="slider-info">
+            <h1 className="slider-title">{title}</h1>
+            <p>{subtitle}</p>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="slider-info">
+            <h1 className="slider-title">{title}</h1>
+            <p>{subtitle}</p>
+          </div>
+          <img src={imageSrc} alt="Emily_Picture" className="imgOne" />
+        </>
+      );
+    }
+  };
 
-export default Slider
+  return <div className="slider">{renderContent()}</div>;
+};
+
+export default Slider;
