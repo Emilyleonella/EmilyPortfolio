995adcd34b4c5181e21b93c0e65e45f82e6031d4
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-15 21:11:07 -0400

    added another image to asset for slider and added more styling

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import Hero from "../components/Hero";
 import heroimage from "../assests/heroImg.jpg";
-import myPic from "../assests/Emily.jpeg"
-import Slider from '../components/Slider';
+import myPic from "../assests/Emily.jpeg";
+import Slider from "../components/Slider";
+import myPic2 from "../assests/Emily2.jpeg"
 
 const Homepage = () => {
   return (
     <div className="App">
       <Hero imageSrc={heroimage} />
+      <Slider
+        imageSrc={myPic}
+        title={"About Me"}
+        subtitle={
+          "dwfwehfowhnwijf fjwief we fowe fwoe fwoef pe fpwe fjof wpe jfpw f"
+        }
+      />
+      <Slider
+        imageSrc={myPic2}
+        title={"About Me"}
+        subtitle={
+          "dwfwehfowhnwijf fjwief we fowe fwoe fwoef pe fpwe fjof wpe jfpw f"
+        }
+        flipped={true}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
