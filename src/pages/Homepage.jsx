868556dc46ae0ea3b5d404bbb55324db75f63459
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-15 20:34:25 -0400

    added more styling and imported my picture and slider to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import heroimage from "../assests/heroImg.jpg";
+import myPic from "../assests/Emily.jpeg"
+import Slider from '../components/Slider';
 
 const Homepage = () => {
   return (
