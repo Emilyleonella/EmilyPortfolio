28de66bcb9f9030333cd96a78a046680c0e670d2
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-19 13:29:57 -0400

    adding scss and images

---
+++ src/components/HeroTwo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroTwo = ({ img }) => {
+  return (
+    <div className="hero-two">
+      <img src={img} alt="code" className="hero-img-two" />
+    </div>
+  );
+};
+
+export default HeroTwo;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,10 +3,13 @@ import Slider from '../components/Slider'
 import gateway from '../assests/Gateway_home.png'
 import pennyjuice from '../assests/Pennyjuice.png'
 import ecom from '../assests/ecom.png'
+import heroCode from '../assests/coding.jpg'
+import HeroTwo from '../components/HeroTwo'
 
 const Project = () => {
   return (
     <div className='App'>
+      <HeroTwo imageSrc={heroCode} />
       <Slider
         imageSrc={gateway}
         title={"Gateway Project"}
