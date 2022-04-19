964e60e26d8d6c2e5687ebd912f6f783b5564429
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-19 14:21:17 -0400

    adding images for possible hero on project page

--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -4,6 +4,7 @@ const HeroTwo = ({ img }) => {
   return (
     <div className="hero-two">
       <img src={img} alt="code" className="hero-img-two" />
+      <h1 className='hero-title'>Welcome to my Portfolio</h1>
     </div>
   );
 };
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,18 +3,34 @@ import Slider from '../components/Slider'
 import gateway from '../assests/Gateway_home.png'
 import pennyjuice from '../assests/Pennyjuice.png'
 import ecom from '../assests/ecom.png'
-import heroCode from '../assests/coding.jpg'
+import heroCode from '../assests/laptop-code.jpg'
 import HeroTwo from '../components/HeroTwo'
 
 const Project = () => {
   return (
     <div className='App'>
-      <HeroTwo imageSrc={heroCode} />
+      {/* <HeroTwo img={heroCode} /> */}
+      <h1 className='Title'>My Projects</h1>
       <Slider
         imageSrc={gateway}
         title={"Gateway Project"}
         subtitle={
           <a className='web-link' href="https://gateway-project.emilyleonella.repl.co/index.html"> Check it out</a>
+        }
+      />
+      <Slider
+        imageSrc={pennyjuice}
+        title={"Revision Project"}
+        subtitle={
+          <a className='web-link' href="https://avpenny.herokuapp.com/contact"> Check it out</a>
+        }
+        flipped={true}
+      />
+      <Slider
+        imageSrc={ecom}
+        title={"E-commerce Project"}
+        subtitle={
+          <a className='web-link'> Check it out</a>
         }
       />
 
