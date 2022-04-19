26d333a33b674738e860762af19487a4a3664c26
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-19 14:31:06 -0400

    added updated about me and changed background color for h1 on project page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import heroimage from "../assests/heroImg.jpg";
 import myPic from "../assests/Emily.jpeg";
 import Slider from "../components/Slider";
-import myPic2 from "../assests/Emily2.jpeg"
+import myPic2 from "../assests/Emily2.jpeg";
 
 const Homepage = () => {
   return (
@@ -13,15 +13,13 @@ const Homepage = () => {
         imageSrc={myPic}
         title={"About Me"}
         subtitle={
-          "dwfwehfowhnwijf fjwief we fowe fwoe fwoef pe fpwe fjof wpe jfpw f"
+          "I am a Coding Apprentice for Road to Hire who is currently falling in love with the Language of Software Development. I am Innovative, Highly Motivated, Bilingual, and Creative. Experienced in Front-end and Back-end development, and with all stages of the Development Life Cycle for Dynamic Software projects."
         }
       />
       <Slider
         imageSrc={myPic2}
         title={"About Me"}
-        subtitle={
-          "Hello World"
-        }
+        subtitle={"Hello World"}
         flipped={true}
       />
     </div>
