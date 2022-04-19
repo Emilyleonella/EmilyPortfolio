a8fbf9a8b9c88d3b8410942c18a573e5ff18a4a5
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-19 13:20:32 -0400

    adding to project page and new hero

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Slider from '../components/Slider'
+import gateway from '../assests/Gateway_home.png'
+import pennyjuice from '../assests/Pennyjuice.png'
+import ecom from '../assests/ecom.png'
 
 const Project = () => {
   return (
     <div className='App'>
-      <Slider/>
+      <Slider
+        imageSrc={gateway}
+        title={"Gateway Project"}
+        subtitle={
+          <a className='web-link' href="https://gateway-project.emilyleonella.repl.co/index.html"> Check it out</a>
+        }
+      />
 
 
     </div>
