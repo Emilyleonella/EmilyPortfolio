2ef96748b2d414a8dd492cbb3b133eb340493f48
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-20 15:41:36 -0400

    fixed arrow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = ({imageSrc}) => {
     <div className='hero'>
       <img src={imageSrc} alt="desk_setup" className='hero-img'/>
       <h1 className='hero-title'>Welcome to my Portfolio</h1>
-      <div><FiChevronsDown  className='arrow-down'/></div>
+      <fig><FiChevronsDown  className='arrow-down'/></fig>
     </div>
   )
 }
