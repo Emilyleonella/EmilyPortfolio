4ee4b7f916e68d65a4296d8b61f2ef98a5b788d2
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-20 15:30:35 -0400

    added arrown down in homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import { FiChevronsDown } from "react-icons/fi";
 const Hero = ({imageSrc}) => {
   return (
     <div className='hero'>
       <img src={imageSrc} alt="desk_setup" className='hero-img'/>
       <h1 className='hero-title'>Welcome to my Portfolio</h1>
-
+      <div><FiChevronsDown  className='arrow-down'/></div>
     </div>
   )
 }
